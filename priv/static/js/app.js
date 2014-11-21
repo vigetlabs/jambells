7feca5e8d85c38acda9e8fcdfcde80cd4c0e5298
Lawson Kurtz <lawson.kurtz@viget.com>
2014-11-21 14:08:44 -0700

    Add pretty bell sounds instead of beeps

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1,5 +1,6 @@
 var socket = new Phoenix.Socket("ws://" + location.host + "/ws");
 var audioContext = new AudioContext()
+
 var userToken = Math.random().toString(36).substr(2)
 
 var bell = new Bell(audioContext)
@@ -32,18 +33,12 @@ socket.join("room", $("#room-info").data("id").toString(), {}, function(chan){
   })
 
   chan.on("note:play", function(message) {
-    // I don't think this is necessary, since
-    // we don't want it to ring when the server
-    // responds, but rather immediately when
-    // the user touchstarts or mousedowns
-
-    // bell.ring(message.note)
+    // something
   })
 
   $('body').on('touchstart mousedown', function(){
     var $noteSelection = $('#noteSelection')
-    var note = parseInt($noteSelection.val())
-    chan.send("note:send", {note: note})
+    var note = $noteSelection.val()
     bell.ring(note)
   })
 

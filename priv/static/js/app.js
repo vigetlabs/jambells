52768d1bef8547593faa2a32bd1027040409d233
Lawson Kurtz <lawson.kurtz@viget.com>
2014-11-21 14:08:44 -0700

    Add pretty bell sounds instead of beeps

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1,9 +1,8 @@
 var socket = new Phoenix.Socket("ws://" + location.host + "/ws");
 var audioContext = new AudioContext()
-
 var bell = new Bell(audioContext)
 
-socket.join("room", $("#room-info").data("id").toString(), {}, function(chan){
+socket.join("room", $("#room-info").data("id").toString(), {}, function(chan) {
   chan.send("user:joined", {})
 
   chan.on("room:update", function(message){
@@ -17,19 +16,9 @@ socket.join("room", $("#room-info").data("id").toString(), {}, function(chan){
     chan.send("user:ready", {})
   })
 
-  chan.on("note:play", function(message) {
-    // I don't think this is necessary, since
-    // we don't want it to ring when the server
-    // responds, but rather immediately when
-    // the user touchstarts or mousedowns
-
-    // bell.ring(message.note)
-  })
-
   $('body').on('touchstart mousedown', function(){
     var $noteSelection = $('#noteSelection')
-    var note = parseInt($noteSelection.val())
-    chan.send("note:send", {note: note})
+    var note = $noteSelection.val()
     bell.ring(note)
   })
 
--- priv/static/js/ring.js
+++ priv/static/js/ring.js
@@ -1,30 +1,70 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext
 
+function Note(audioFile) {
+  this.audioFileURL = "http://" + location.host + audioFile
+  this.loaded = false
+}
+
+Note.prototype = {
+  play: function(audioContext) {
+    if (this.loaded) {
+      this.playInContext(audioContext)
+    } else {
+      this.loadSound(audioContext)
+    }
+  },
+
+  playInContext: function(audioContext) {
+    var source = audioContext.createBufferSource()
+    source.buffer = this.buffer
+    source.connect(audioContext.destination);
+    source.start(0)
+  },
+
+  loadSound: function(audioContext) {
+    var note = this
+    var request = new XMLHttpRequest();
+    request.open('GET', this.audioFileURL, true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = function() {
+      audioContext.decodeAudioData(request.response, function(buffer) {
+        note.buffer = buffer
+        note.loaded = true
+        note.play(audioContext)
+      }, function() { alert('This bell malfunctioned! Reload the page.') })
+    }
+    request.send()
+  }
+}
+
 Bell = function(audioContext) {
   this.audioContext = audioContext
+  this.memoizedNotes = null
 }
 
 Bell.prototype = {
-  ringDuration: 100,
+  ring: function(noteSelection) {
+    note = this.notes()[noteSelection]
 
-  ring: function(frequency) {
-    this.oscillator =  this.audioContext.createOscillator()
-    this.setOscillatorType('square')
-    this.setOscillatorFrequency(frequency)
-    this.oscillator.start(0)
-    this.oscillator.connect(this.audioContext.destination)
-    setTimeout(this.mute.bind(this), this.ringDuration);
+    if (note) {
+      note.play(this.audioContext)
+    }
   },
 
-  mute: function() {
-    this.oscillator.stop(0)
-  },
+  notes: function() {
+    if (this.memoizedNotes === null) {
+      this.memoizedNotes = {
+        a: new Note("/sounds/bell-A.wav"),
+        b: new Note("/sounds/bell-B.wav"),
+        c: new Note("/sounds/bell-C.wav"),
+        d: new Note("/sounds/bell-D.wav"),
+        e: new Note("/sounds/bell-E.wav"),
+        f: new Note("/sounds/bell-F.wav"),
+        g: new Note("/sounds/bell-G.wav")
+      }
+    }
 
-  setOscillatorType: function(type) {
-    this.oscillator.type = type
-  },
-
-  setOscillatorFrequency: function(frequency) {
-    this.oscillator.frequency.value = frequency
+    return this.memoizedNotes
   }
 }

f3477e86a79e54315a3546d6ddefb25df0b482b2
Eli Fatsi <eli.fatsi@viget.com>
2014-11-21 20:42:14 -0700

    Added potential for cat tones

--- priv/static/js/ring.js
+++ priv/static/js/ring.js
@@ -70,6 +70,22 @@ Bell.prototype = {
         a4: new Note("/sounds/bell-A4.wav"),
         b4: new Note("/sounds/bell-B4.wav"),
         c5: new Note("/sounds/bell-C5.wav"),
+
+        // c3: new Note("/sounds/cat-C3.wav"),
+        // d3: new Note("/sounds/cat-D3.wav"),
+        // e3: new Note("/sounds/cat-E3.wav"),
+        // f3: new Note("/sounds/cat-F3.wav"),
+        // g3: new Note("/sounds/cat-G3.wav"),
+        // a3: new Note("/sounds/cat-A3.wav"),
+        // b3: new Note("/sounds/cat-B3.wav"),
+        // c4: new Note("/sounds/cat-C4.wav"),
+        // d4: new Note("/sounds/cat-D4.wav"),
+        // e4: new Note("/sounds/cat-E4.wav"),
+        // f4: new Note("/sounds/cat-F4.wav"),
+        // g4: new Note("/sounds/cat-G4.wav"),
+        // a4: new Note("/sounds/cat-A4.wav"),
+        // b4: new Note("/sounds/cat-B4.wav"),
+        // c5: new Note("/sounds/cat-C5.wav"),
       }
     }
 

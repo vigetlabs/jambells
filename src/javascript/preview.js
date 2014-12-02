c1c27b929109a4f6a4645b15f67b5185350cc2c1
Tommy Marshall <tommy.marshall@viget.com>
2014-12-02 11:43:15 -0500

    Adds check for song data.

--- src/javascript/preview.js
+++ src/javascript/preview.js
@@ -6,27 +6,30 @@ var bell         = new Bell(audioContext)
 
 var song_info   = $("#room-info").data("song")
 
-var notes       = song_info.notes
-var miliPerBeat = 60000 / song_info.bpm
+if (song_info) {
+  var notes       = song_info.notes
+  var miliPerBeat = 60000 / song_info.bpm
 
-var index = 0
+  var index = 0
 
-var playNote = function() {
-  console.log(notes[index])
+  var playNote = function() {
+    console.log(notes[index])
 
-  bell.ring(notes[index].toLowerCase())
+    bell.ring(notes[index].toLowerCase())
 
-  index++
+    index++
 
-  if (index < notes.length) {
-    setTimeout(playNote, miliPerBeat)
-  } else {
-    console.log("Song over")
+    if (index < notes.length) {
+      setTimeout(playNote, miliPerBeat)
+    } else {
+      console.log("Song over")
+    }
   }
+
+  $("#preview-play").click(function(e) {
+    e.preventDefault()
+
+    playNote()
+  })
+
 }
-
-$("#preview-play").click(function(e) {
-  e.preventDefault()
-
-  playNote()
-})

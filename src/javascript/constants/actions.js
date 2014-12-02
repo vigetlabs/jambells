d901858589a8d59b0dc53d9c7a0cc2394beae2dd
Tommy Marshall <tommy.marshall@viget.com>
2014-12-02 17:48:45 -0500

    Removes deprecated merge. Removes pause. Combines setPlayerNote and play actions.

--- src/javascript/constants/actions.js
+++ src/javascript/constants/actions.js
@@ -2,8 +2,6 @@ var keyMirror = require('react/lib/keyMirror')
 
 module.exports = keyMirror({
 
-  PLAY_SONG       : null,
-  PAUSE_SONG      : null,
-  SET_PLAYER_NOTE : null
+  PLAY_SONG : null
 
 });

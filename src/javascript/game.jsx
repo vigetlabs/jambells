1848f18934356dabe1b94c575b151b0f6681c4de
Tommy Marshall <tommy.marshall@viget.com>
2014-12-02 17:00:22 -0500

    General alignment and cleanup.

--- src/javascript/game.jsx
+++ src/javascript/game.jsx
@@ -11,18 +11,18 @@ var Phoenix        = require('./vendor/phoenix')
 var ComputerPlayer = require('./util/computer_player')
 var SongActions    = require('./actions/song')
 
-var ringDebounceDelay = false;
+var ringDebounceDelay = false
 
 var Game = function(container) {
   this.container = container
   this.cacheDom()
-  this.data = this.$lobby.data()
-  this.note = this.$noteSelection.val()
-  this.bell = new Bell( new AudioContext() )
+  this.data         = this.$lobby.data()
+  this.note         = this.$noteSelection.val()
+  this.bell         = new Bell( new AudioContext() )
   this.computerBell = new ComputerPlayer(this.bell)
-  this.userToken = Math.random().toString(36).substr(2)
-  this.gameLeader = false
-  this.attachSong();
+  this.userToken    = Math.random().toString(36).substr(2)
+  this.gameLeader   = false
+  this.attachSong()
   this.connect()
 }
 
@@ -32,33 +32,33 @@ Game.prototype = {
   },
 
   connect: function() {
-    var socket = new Phoenix.Socket("ws://" + location.host + "/ws");
-    socket.join("room", this.data.id.toString(), {}, this.setup.bind(this))
+    var socket = new Phoenix.Socket('ws://' + location.host + '/ws')
+    socket.join('room', this.data.id.toString(), {}, this.setup.bind(this))
   },
 
   cacheDom: function() {
-    this.$lobby         = $("#game-lobby")
+    this.$lobby         = $('#game-lobby')
     this.$noteSelection = $('#noteSelection')
-    this.$readyButton   = $("#ready-button")
-    this.$startButton   = $("#start-game")
-    this.$game          = $("#game")
-    this.$usersPresent  = $("#users-present")
-    this.$usersReady    = $("#users-ready")
+    this.$readyButton   = $('#ready-button')
+    this.$startButton   = $('#start-game')
+    this.$game          = $('#game')
+    this.$usersPresent  = $('#users-present')
+    this.$usersReady    = $('#users-ready')
   },
 
   start: function(e) {
     if (this.$startButton.not(':disabled')) {
       e.preventDefault()
-      this.chan.send("game:start", {})
+      this.chan.send('game:start', {})
     }
   },
 
   ring: function() {
     if (!ringDebounceDelay) {
-      ringDebounceDelay = true;
+      ringDebounceDelay = true
       setTimeout(function(){
-        ringDebounceDelay = false;
-      }, 100);
+        ringDebounceDelay = false
+      }, 100)
 
       this.bell.ring(this.note)
     }
@@ -72,22 +72,23 @@ Game.prototype = {
     this.$readyButton.click(this.announceReady.bind(this))
     this.$startButton.click(this.start.bind(this))
     this.$game.on('touchstart mousedown', this.ring.bind(this))
-    this.chan.send("user:joined", {user_token: this.userToken})
-    this.chan.on("room:update", this.renderRoomInfo.bind(this))
-    this.chan.on("game:started", this.renderGame.bind(this))
+    this.chan.send('user:joined', {user_token: this.userToken})
+    this.chan.on('room:update', this.renderRoomInfo.bind(this))
+    this.chan.on('game:started', this.renderGame.bind(this))
   },
 
   announceReady: function(e) {
     e.preventDefault()
     this.$readyButton.hide()
     this.$startButton.show()
-    this.chan.send("user:ready", {})
+    this.chan.send('user:ready', {})
   },
 
   renderGame: function(message) {
     this.$lobby.hide()
     this.$game.show()
-    SongActions.play();
+
+    SongActions.play()
     SongActions.setPlayerNote(message.user_info.indexOf(this.userToken))
 
     if (this.gameLeader && this.ready < this.data.song.roles.length) {
@@ -115,7 +116,7 @@ Game.prototype = {
 
     // TODO: This should be a react view probably
     this.note = this.data.song.roles[userInfo.indexOf(this.userToken)].toLowerCase()
-    this.$noteSelection.val(this.note);
+    this.$noteSelection.val(this.note)
     this.$usersPresent.html(present)
     this.$usersReady.html(this.ready)
   }

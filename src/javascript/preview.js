64166d85797076648e4e5fb231eb4bc2f3c099d0
Tommy Marshall <tommy.marshall@viget.com>
2014-12-04 14:24:18 -0500

    Adds back in refactored Preview code.

--- src/javascript/preview.js
+++ src/javascript/preview.js
@@ -1,35 +1,38 @@
-var Bell         = require('./lib/Bell')
-var audioContext = new AudioContext()
+// Fix this if anyone cares at some point...
+// #TODO - i broke this. Love, Dan
 
-var Preview = function(el) {
-  this.el      = el
-  this.preview = document.querySelector('#preview-play')
-  this.bell    = new Bell(audioContext)
-  this.song    = JSON.parse(this.el.getAttribute('data-song'))
-  this.notes   = this.song.notes
-  this.bindEvents()
-};
+// var Bell         = require('./lib/Bell')
+// var audioContext = new AudioContext()
 
-Preview.prototype = {
-  bindEvents: function() {
-    this.preview.addEventListener('click', this.playNotes.bind(this))
-  },
+// var Preview = function(el) {
+//   this.el      = el
+//   this.preview = document.querySelector('#preview-play')
+//   this.bell    = new Bell(audioContext)
+//   this.song    = JSON.parse(this.el.getAttribute('data-song'))
+//   this.notes   = this.song.notes
+//   this.bindEvents()
+// };
 
-  playNotes: function() {
-    this.notes.forEach(function(note_info){
-      var note  = note_info.note.toLowerCase()
-      var delay = note_info.milliseconds_from_start
+// Preview.prototype = {
+//   bindEvents: function() {
+//     this.preview.addEventListener('click', this.playNotes.bind(this))
+//   },
 
-      setTimeout(this.ringBellNote(note).bind(this), delay)
-    }.bind(this))
-  },
+//   playNotes: function() {
+//     this.notes.forEach(function(note_info){
+//       var note  = note_info.note.toLowerCase()
+//       var delay = note_info.milliseconds_from_start
 
-  ringBellNote: function(note) {
-    return function() {
-      console.log('Play', note)
-      this.bell.ring(note)
-    }.bind(this)
-  }
-}
+//       setTimeout(this.ringBellNote(note).bind(this), delay)
+//     }.bind(this))
+//   },
 
-module.exports = Preview
+//   ringBellNote: function(note) {
+//     return function() {
+//       console.log('Play', note)
+//       this.bell.ring(note)
+//     }.bind(this)
+//   }
+// }
+
+// module.exports = Preview

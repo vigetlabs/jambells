12341c69c9d780780fb1c12782a353e1218bc918
Megan Zlock <zlockart@gmail.com>
2014-12-08 12:49:39 -0500

    styled song select

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,6 +2,34 @@ var $       = require('jquery')
 var Game    = require('./game')
 var Preview = require('./preview')
 
+var songSelect = function() {
+  var $container = $('.song-list');
+  var $songs = $container.find('li');
+  var $form = $('form[action="/rooms"]');
+  var $select = $form.find('select[name="room[song]"]');
+
+  var pickSong = function() {
+    var $song = $(this);
+    $song.addClass('-selected');
+    var songName = $song.data('select');
+    $select.val(songName);
+    $form.submit();
+  }
+
+  var addEvents = function() {
+    $songs.on('click', pickSong);
+  }
+
+  return {
+    init: function() {
+      addEvents();
+    }
+  }
+}();
+
+songSelect.init();
+
+
 // js for previewing songs in full
 var previewNode = document.querySelector('#room-info')
 

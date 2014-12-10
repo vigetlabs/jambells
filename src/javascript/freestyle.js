c9e28a8af3479f68b8cc4cb3bc20d7e19cfceec8
Tommy Marshall <tommy.marshall@viget.com>
2014-12-10 16:53:28 -0500

    Adds Tap dialog to load note on user input.

--- src/javascript/freestyle.js
+++ src/javascript/freestyle.js
@@ -1,6 +1,8 @@
-var $        = require('jquery')
-var HandBell = require('./lib/Handbell')
-var respondToEvent = ('ontouchend' in document) ? 'touchstart' : 'click'
+var $              = require('jquery')
+var HandBell       = require('./lib/Handbell')
+var isTouch        = ('ontouchend' in document)
+var respondToEvent = isTouch ? 'touchstart' : 'click'
+
 var Freestyle = function() {
   this.initialize()
 }
@@ -8,28 +10,42 @@ var Freestyle = function() {
 Freestyle.prototype = {
   initialize: function() {
     this.$buttons = $('.freestyle-note-button')
-    this.$body    = $('body')
+    this.$play    = $('#play-now')
 
     this.setupHandbell()
+
+    if (isTouch) {
+      this.$play.addClass('-show')
+      this.$play.on(respondToEvent, function(e){
+        this.handBell.ding.call(this.handBell)
+        this.$play.removeClass('-show')
+      }.bind(this))
+    }
+
+    this.$buttons.on(respondToEvent, this.updateHandbell.bind(this))
   },
 
   setupHandbell: function() {
     var note = this.$buttons.filter('.-active').data('note')
+    this.savedNote = note
 
     this.handBell = new HandBell(note)
     this.handBell.initialize()
-    this.$buttons.on(respondToEvent, this.updateHandbell.bind(this))
-    this.$buttons.on(respondToEvent, this.handBell.ding.bind(this.handBell))
   },
 
   updateHandbell: function(e) {
     var $el = $(e.target)
     var note = $el.data('note')
 
-    this.$buttons.removeClass('-active')
+    this.$buttons.removeClass('-active -fill')
     $el.addClass('-active')
 
-    this.handBell.setNote(note)
+    if (this.savedNote != note) {
+      this.savedNote = note
+      this.handBell.setNote(note)
+    }
+
+    this.handBell.ding()
   }
 }
 

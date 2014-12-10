aec09a6bf4009b76d3196ab929d19f7291acc569
Megan Zlock <zlockart@gmail.com>
2014-12-10 06:37:23 -0500

    starting hand animation

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,12 +2,13 @@ var $         = require('jquery')
 var Game      = require('./game')
 var Preview   = require('./preview')
 var Freestyle = require('./freestyle')
+var Cookies   = require('./util/cookies')
 
 var songSelect = function() {
   var $container = $('.song-list');
-  var $songs = $container.find('li');
-  var $form = $('form[action="/rooms"]');
-  var $select = $form.find('select[name="room[song]"]');
+  var $songs     = $container.find('li');
+  var $form      = $('form[action="/rooms"]');
+  var $select    = $form.find('select[name="room[song]"]');
 
   var pickSong = function() {
     var $song = $(this);
@@ -23,22 +24,74 @@ var songSelect = function() {
 
   return {
     init: function() {
-      addEvents();
+      if($container.length > 0) {
+        addEvents();
+      }
+    }
+  }
+}();
+
+var instructScreen = function() {
+  var $container      = $('.home');
+  var $buttons        = $container.find('.actions .button');
+  var $instructions   = $container.find('#instructions');
+  var $instructButton = $instructions.find('.button');
+  var toURL           = '';
+  var cookieName      = 'jb_return_visit';
+
+  var showInstructions = function() {
+    $container.removeClass('home').addClass('instruct-screen');
+  }
+
+  var startSound = function() {
+    var freestyle = new Freestyle()
+  }
+
+  var setCookie = function() {
+
+  }
+
+  var checkCookie = function(e) {
+    if(Cookies.read(cookieName) == null) {
+      toURL = $(this).attr('href');
+      showInstructions();
+      startSound();
+      setCookie();
+      e.preventDefault();
+    }
+  }
+
+  var reroute = function() {
+    window.location.replace(toURL);
+  }
+
+  var addEvents = function() {
+    $buttons.on('click', checkCookie);
+    $instructButton.on('click', reroute);
+  }
+
+  return {
+    init: function() {
+      if($buttons.length > 0) {
+        addEvents();
+      }
     }
   }
 }();
 
 songSelect.init();
+instructScreen.init();
+
 
 // js for freestyling
 var freestyleNode = document.querySelector('.freestyle-note-button')
-
 if (freestyleNode) new Freestyle()
+
 
 // js for previewing songs in full
 var previewNode = document.querySelector('#preview-room-info')
-
 if (previewNode) new Preview(previewNode)
+
 
 // Index page
 // #TODO Add basic routing
@@ -51,9 +104,9 @@ $roomCode.on('input', function(e) {
   $roomLink.attr('href', '/' + $(this).val())
 })
 
+
 // Game page
 var container = document.getElementById('game')
-
 if (container) {
   window.game = new Game(container)
 }
